53d052319aa63c966a70ecacecb0058f4e4f56e6
Alexandr <theobroma222@gmail.com>
2020-11-03 16:55:21 +0200

    forecastday

--- src/@components/Forecast/Forecast.tsx
+++ src/@components/Forecast/Forecast.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { currentWeatherSelector } from '../../@store/current-weather/selectors';
+import { forecastdaySelector } from '../../@store/forecast/selectors';
 import { getForecastTC } from '../../@store/forecast/slice';
+import { ForecastdayResponseType } from '../../@types';
+import ForecastDay from './ForecastDay/ForecastDay';
 
-const Forecast = () => {
+const Forecast: React.FC = () => {
   const dispatch = useDispatch();
   const { lon, lat } = useSelector(currentWeatherSelector);
+  const forecastInfo = useSelector(forecastdaySelector);
   const forecastDays = 3;
 
   useEffect(() => {
@@ -14,9 +18,31 @@ const Forecast = () => {
     }
   }, [lat, lon, forecastDays]);
 
+  const currentWeekday = { weekday: 'long' };
+  const currentDate = { year: 'numeric', month: 'long', day: 'numeric' };
+
   return (
     <div>
       <span>Forecast</span>
+      <div>
+        {forecastInfo.map((d: ForecastdayResponseType) => {
+          const adaptedDate = new Date(d.date);
+          return (
+            <div key={d.date}>
+              <ForecastDay
+                weekDay={adaptedDate.toLocaleString('en-US', currentWeekday)}
+                date={adaptedDate.toLocaleString('en-US', currentDate)}
+                sunrise={d.astro.sunrise}
+                sunset={d.astro.sunset}
+                icon={d.day.condition.icon}
+                condition_text={d.day.condition.text}
+                min_temp={d.day.mintemp_c}
+                max_temp={d.day.maxtemp_c}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/@types/General.ts
+++ src/@types/General.ts
@@ -1,1 +1,15 @@
 export type FilterType = 'SHOW_ALL' | 'SHOW_ACTIVE' | 'SHOW_COMPLETED';
+
+export type ForecastDayInfoType = {
+  forecastday: Array<ForecastdayResponseType>;
+};
+
+export type ForecastdayResponseType = {
+  date: string;
+  //   day: DayResponseType;
+  day: any;
+  //   astro: AstroResponseType;
+  astro: any;
+  //   hour: Array<HourResponseType>;
+  hour: any;
+};
